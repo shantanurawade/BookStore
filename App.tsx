9cb8bc67274110c8b17b6c8487aba089909ad7c6
shantanurawade <rawadeshantanu@gmail.com>
2024-11-25 14:05:56 +0530

    [Shantanu ] improved : app.tsx as per updations.

--- App.tsx
+++ App.tsx
@@ -2,27 +2,34 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React, { useEffect, useState } from 'react'
 import { NavigationContainer } from "@react-navigation/native";
 import MainScreen from "./src/screens/MainScreen";
-import Login from "./src/screens/Authentication/Login";
-import SignUp from "./src/screens/Authentication/SignUp";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { ActivityIndicator } from "react-native-paper";
 import { View } from "react-native";
+import Authentication from "./src/screens/Authentication/Authentication";
+import { useSelector } from "react-redux";
+import { RootState } from "./src/redux/Store";
+
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-  const [user, setUser] = useState<any>(null);
+  const [token, setToken] = useState<any>(null);
   const [isLoading, setLoading] = useState(true);
+  const authStatus = useSelector((store: RootState) => store.auth.toggle);
+
+  // Check wether user is already logged in or not.
   useEffect(() => {
+
     ; (async () => {
-      const data = await AsyncStorage.getItem("currentUser");
-      setUser(data);
+      const data = await AsyncStorage.getItem("userToken");
+      setToken(data);
       setLoading(false);
     }
     )();
 
-  }, [isLoading])
+  }, [isLoading, authStatus]);
 
+  // While checking existing user it will show the activity indicator.
   if (isLoading) {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -30,16 +37,16 @@ const App = () => {
       </View>
     )
   }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {user ?
-          (<Stack.Screen component={MainScreen} name="Mainscreen" options={{ headerShown: false }} />) :
-          (<><Stack.Screen component={Login} name="Login" options={{ headerShown: false }}></Stack.Screen>
-            <Stack.Screen component={SignUp} name="SignUp" options={{ headerShown: false }}></Stack.Screen></>)
+        {token ?
+          (<Stack.Screen component={MainScreen} name="MainScreen" options={{ headerShown: false }} />) :
+          (<Stack.Screen component={Authentication} name="Authentication" options={{ headerShown: false }}></Stack.Screen>)
         }
       </Stack.Navigator>
     </NavigationContainer>)
 }
 
-export default App
+export default App;
