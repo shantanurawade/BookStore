3d6ac505277c8849be40d5dbe221afe1742ba888
shantanurawade <rawadeshantanu@gmail.com>
2024-11-25 14:06:31 +0530

    [Shantanu ] improved : for providing store.

--- index.js
+++ index.js
@@ -2,9 +2,22 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
 import App from './App';
+import React from 'react'
+import { Provider } from 'react-redux';
+import store from './src/redux/Store';
 
-AppRegistry.registerComponent(appName, () => App);
+const index = () => {
+    return (
+        <Provider store={store} >
+            <App />
+        </Provider >
+    )
+}
+
+export default index
+
+AppRegistry.registerComponent(appName, () => index);
 
