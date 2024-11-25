3aa61a3731437a0d72d5f0d55b2366866a6b85be
shantanurawade <rawadeshantanu@gmail.com>
2024-11-25 14:02:08 +0530

    [Shantanu ] Update : updated screens for empty cart option.

--- src/screens/Cart/Cart.tsx
+++ src/screens/Cart/Cart.tsx
@@ -1,31 +1,32 @@
-import { View, Text, FlatList, Pressable, Image, LogBox } from 'react-native'
+import { View, Text, FlatList, Pressable, Image } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { style } from '../../styles/globalStyles'
 import axios from 'axios'
-import { book } from '../MainScreen'
 import { AddressCart, CartTotal } from './CartUtils'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { SERVER_URL } from '../../Constants/constant'
+import PaymentSuccess from '../PaymentSuccess'
 
 
-const Cart = () => {
+const Cart = (props: any) => {
     const [cartData, setCartData] = useState<any[]>([]);
     const [count, setCount] = useState(0);
     const [amount, setAmount] = useState(0);
-
+    const [orderPlaced, placeOrder] = useState(false);
 
     useEffect(() => {
+        // It will act like temperary array to store array in setCartData as useState in asynchronous and may cause unexpected effect.
         var cartDataArray: any = [];
         ; (async () => {
             try {
-                const dataUser = await AsyncStorage.getItem("currentUser");
-                let user;
-                if (dataUser) { user = JSON.parse(dataUser); }
-                const data = await (axios.get(`http://10.0.2.2:8000/api/v1/user/get/${user._id}`));
+                const userToken = await AsyncStorage.getItem("userToken");
+                const data = await (axios.get(`${SERVER_URL}/user/get`,
+                    { headers: { Authorization: `Bearer ${userToken}` } }));
                 const cartItems: [] = data.data.cart.items;
                 await Promise.all(
                     cartItems.map(async (items: any) => {
                         const getBook = items.bookId;
-                        const book = await axios.get(`http://10.0.2.2:8000/api/v1/book/getById/${getBook}`);
+                        const book = await axios.get(`${SERVER_URL}/book/getById/${getBook}`);
                         const cart = book.data.data;
                         const quantity = items.quantity
                         cartDataArray.push({ ...cart, quantity });
@@ -33,58 +34,73 @@ const Cart = () => {
                 setCartData(cartDataArray);
                 setCount(cartDataArray.length);
                 setAmount(data.data.cart.totalAmount);
+                console.log(cartData.length)
             } catch (error) {
                 console.warn(error);
             }
         })();
-
-
     }, []);
     return (
-        <View style={{ marginTop: 8, marginBottom: 140 }}>
-            <Text style={{ fontSize: 16 }}>Books
-                <Text style={style.booksCount}>({count})</Text>
-            </Text>
-            {cartData.length > 0 ?
-                <FlatList
-                    data={cartData}
-                    // numColumns={2}
-                    ListFooterComponent={<View>
-                        <AddressCart />
-                        <CartTotal amount={amount} />
-                    </View>}
-                    contentContainerStyle={{ paddingBottom: 150 }}
-                    keyExtractor={(item) => item._id.toString()}
-                    renderItem={({ item }) => (
-                        <View style={style.bookCardCart}>
-                            <View style={{ flexDirection: 'row' }}>
-                                <Image style={[style.image, { margin: 20 }]} source={{ uri: `${item.img}` }} />
-                                <View style={{ marginTop: 20 }}>
-                                    <Text numberOfLines={2} style={{ fontSize: 20, width: 200 }}>{item.title}</Text>
-                                    <Text style={{ color: 'grey' }}>~ By {item.author}</Text>
-                                    <Text style={[style.priceText, { marginTop: 20 }]}>Rs.{item.discountedPrice}
-                                        <Text style={style.priceTextDiscounted}>Rs.{item.price}</Text>
-                                    </Text>
-                                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                        <Pressable>
-                                            <Text style={style.roundButtonMarron}>-</Text>
-                                        </Pressable>
-                                        <Text style={{ textAlign: 'center', marginHorizontal: 10 }}>{item.quantity}</Text>
-                                        <Pressable>
-                                            <Text style={style.roundButtonMarron}>+</Text>
-                                        </Pressable>
-                                    </View>
+        <>
+            {
+                orderPlaced ?
+                    <PaymentSuccess setCart={props.setCart} /> : < View style={{ marginTop: 8, marginBottom: 140 }
+                    }>
+                        {
+                            cartData.length >= 1 ? <>
+                                <Text style={{ fontSize: 16 }}>Books
+                                    <Text style={style.booksCount}>({count})</Text>
+                                </Text>
+                                <FlatList
+                                    data={cartData}
+                                    ListFooterComponent={<View>
+                                        <AddressCart />
+                                        <CartTotal amount={amount} placeOrder={placeOrder} />
+                                    </View>}
+                                    contentContainerStyle={{ paddingBottom: 150 }}
+                                    keyExtractor={(item) => item._id.toString()}
+                                    renderItem={({ item }) => (
+                                        <View style={style.bookCardCart}>
+                                            <View style={{ flexDirection: 'row' }}>
+
+                                                <Image style={[style.image, { margin: 20 }]} source={{ uri: `${item.img}` }} />
+
+                                                <View style={{ marginTop: 20 }}>
+
+                                                    <Text numberOfLines={2} style={{ fontSize: 20, width: 200 }}>{item.title}</Text>
+                                                    <Text style={{ color: 'grey' }}>~ By {item.author}</Text>
+                                                    <Text style={[style.priceText, { marginTop: 20 }]}>Rs.{item.discountedPrice}
+                                                        <Text style={style.priceTextDiscounted}>Rs.{item.price}</Text>
+                                                    </Text>
+
+                                                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+
+                                                        <Pressable>
+                                                            <Text style={style.roundButtonMarron}>-</Text>
+                                                        </Pressable>
+
+                                                        <Text style={{ textAlign: 'center', marginHorizontal: 10 }}>{item.quantity}</Text>
+
+                                                        <Pressable>
+                                                            <Text style={style.roundButtonMarron}>+</Text>
+                                                        </Pressable>
+                                                    </View>
+
+                                                </View>
+                                                <Pressable style={{ position: 'absolute', right: 25, top: 15 }}>
+                                                    <Text style={{ fontSize: 20 }}>X</Text>
+                                                </Pressable>
+
+                                            </View>
+                                        </View>
+                                    )} /> </> :
+                                <View style={{ alignItems: 'center' }}>
+                                    <Image style={{ height: 200, width: 200 }} source={require('../../assests/images/empty-cart.png')} />
+                                    <Text style={{ fontSize: 50, textAlign: 'center' }}>Cart is empty</Text>
                                 </View>
-                                <Pressable style={{ position: 'absolute', right: 25, top: 15 }}>
-                                    <Text style={{ fontSize: 20 }}>X</Text>
-                                </Pressable>
-                            </View>
-                        </View>
-                    )} /> :
-                <View style={{ flex: 1 }}>
-                    <Text>Cart is empty.</Text>
-                </View>}
-        </View>
+                        }
+                    </View >
+            }</>
     )
 }
 
--- src/screens/Cart/CartUtils.tsx
+++ src/screens/Cart/CartUtils.tsx
@@ -1,22 +1,42 @@
-import { useEffect } from "react";
-import { Pressable, Text, View } from "react-native"
+import axios from "axios";
+import { ActivityIndicator, Pressable, Text, View } from "react-native"
+import { SERVER_URL } from "../../Constants/constant";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useState } from "react";
 
 export const CartTotal = (props: any) => {
     const amount = props.amount;
-    console.log('====================================');
-    console.log(amount);
-    console.log('====================================');
+    const [proccessing, setProccessing] = useState(false);
+    const placeOrder = () => {
+        setProccessing(true);
+        setTimeout(() => {
+            ; (async () => {
+                const userToken = await AsyncStorage.getItem('userToken');
+                console.log(userToken);
+                axios.patch(`${SERVER_URL}/user/placeOrder`, {}, { headers: { Authorization: `Bearer ${userToken}` } }).then((res) => {
+                    console.log(res);
+                    props.placeOrder(true);
+                }).catch((err) => {
+                    console.log('ff', err);
+                })
+            })();
+            setProccessing(false);
+        }, 3000)
 
+    }
     return (
         <View style={{ paddingHorizontal: 16, backgroundColor: 'white', alignItems: 'center', justifyContent: 'space-between', height: '26%', flexDirection: 'row', marginTop: 20, width: '100%' }}>
             <View style={{ borderEndWidth: 1, width: 212, borderColor: 'grey' }}>
                 <Text style={{ fontSize: 20 }}>Total</Text>
                 <Text style={{ fontSize: 20, color: '#911c35' }}>Rs. {amount}</Text>
             </View>
-            <Pressable style={{ backgroundColor: '#911c35', height: 60, width: 150, borderRadius: 5, alignContent: 'center', justifyContent: 'center' }}>
-                <Text style={{ textAlign: 'center', color: 'white', fontSize: 16 }} >
-                    PLACE ORDER
-                </Text>
+
+            <Pressable onPress={() => { placeOrder() }} style={{ backgroundColor: '#911c35', height: 60, width: 150, borderRadius: 5, alignContent: 'center', justifyContent: 'center' }}>
+                {
+                    proccessing ? <ActivityIndicator size={'large'} /> : <Text style={{ textAlign: 'center', color: 'white', fontSize: 16 }} >
+                        PLACE ORDER
+                    </Text>
+                }
             </Pressable>
         </View >
     )
